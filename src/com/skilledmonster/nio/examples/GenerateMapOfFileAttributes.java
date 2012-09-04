ffde9d1283bd520192a77cb7b4e32be206ed0a22
Jagamot <Jagamot@ACCJAGAMOTL412.corp.star>
2012-09-04 14:38:10 -0400

    obtain map of file attributes

---
+++ src/com/skilledmonster/nio/examples/GenerateMapOfFileAttributes.java
@@ -0,0 +1,27 @@
+package com.skilledmonster.nio.examples;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Example to obtain file attributes in a map
+ * @author Jagamot
+ */
+public class GenerateMapOfFileAttributes {
+
+    public static void main(String[] args) throws IOException {
+        // create Path object representing a file
+        Path path = FileSystems.getDefault().getPath("c://skilledmonster//pom.xml");
+        // call readAttributes() method and generate the map
+        Map<String, Object> attributesMap = Files.readAttributes(path, "*");
+        Set<String> keys = attributesMap.keySet();
+        for (String attribute : keys) {
+            // display all the file attributes
+            System.out.println(attribute + " : "+ Files.getAttribute(path, attribute));
+        }
+    }
+}
